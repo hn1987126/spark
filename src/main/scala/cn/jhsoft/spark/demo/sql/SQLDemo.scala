849f3cdf6687faeaf9bbae3d91a58870103f6db0
chenyi <chenyi9@jd.com>
2017-07-31 17:39:23 +0800

    sql

---
+++ src/main/scala/cn/jhsoft/spark/demo/sql/SQLDemo.scala
@@ -0,0 +1,37 @@
+package cn.jhsoft.spark.demo.sql
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
+
+/**
+  * Created by chenyi9 on 2017/7/31.
+  */
+object SQLDemo {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("SQLDemo")//.setMaster("local[3]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    // 设置hadoop的启动用户
+    System.setProperty("HADOOP_USER_NAME", "hadoop");
+
+    val personRdd = sc.textFile("hdfs://s1:9000/wordcount/input/1.txt").map(line => {
+      val filed = line.split(",")
+      Person(filed(0).toLong, filed(1), filed(2).toInt)
+    })
+
+    // 隐式转换
+    import sqlContext.implicits._
+    val personDF = personRdd.toDF()
+    personDF.registerTempTable("t_person")
+
+    sqlContext.sql("select * from t_person where age>40").show()
+
+    sc.stop()
+
+  }
+
+}
+
+case class Person(id:Long, name:String, age:Int)
+
--- src/main/scala/cn/jhsoft/spark/demo/wordcount/WordCountRemote.scala
+++ src/main/scala/cn/jhsoft/spark/demo/wordcount/WordCountRemote.scala
@@ -13,8 +13,11 @@ object WordCountRemote {
     // "/Users/chen/java/spark/target/spark-1.0.jar" 这个路径字符串 是点右键 Copy Path来的
 
     val config = new SparkConf().setAppName("WC")
-        .setJars(Array("/Users/chen/java/spark/target/spark-1.0.jar"))
+//        .setJars(Array("/Users/chen/java/spark/target/spark-1.0.jar"))
+        .setJars(Array("D:\\Java\\spark\\target\\spark-1.0.jar"))
         .setMaster("spark://s1:7077")
+    // 设置hadoop的启动用户
+    System.setProperty("HADOOP_USER_NAME", "hadoop");
 
     val sc = new SparkContext(config)
     sc.textFile(args(0)).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).sortBy(_._2, false).saveAsTextFile(args(1))
@@ -22,7 +25,7 @@ object WordCountRemote {
     //textFile方法会产生两个RDD，1、HadoopRDD，2、MapPartitionRDD【调用了spark的map方法，把第一步hadoop返回的偏移量去掉了(hadoop返回了，偏移量和行内容)】
     //flatMap 方法会产生一个RDD   MapPartitionRDD
     //map 方法会产生一个RDD   MapPartitionRDD
-    //reduceByKey 方法会产生一个RDD   ShuffledRDD
+    //reduceByKey 方法会产生一个RDD   ShuffledRDD [下游到上游来摘取数据的过程，那就要等上游的数据处理完才能拉]
     //saveToTextFile  方法产生一个RDD    MapPartitionRDD
 
 
